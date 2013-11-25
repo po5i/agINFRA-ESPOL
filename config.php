9f9d5fd35aa875c34673d79c5c3b41e6370fd6f2
Carlos Villavicencio <carlos.villavicencio@cti.espol.edu.ec>
2013-11-25 15:29:54 -0500

    added config file

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$COUCHDB_AGINFRA = "aginfra_datasets";
+$COUCHDB_AGRIS = "agris_testing";
+
+//$COUCHDB_AGINFRA = "aginfra";
+//$COUCHDB_AGRIS = "agris";
+
+//SNV
+$COUCH = "localhost:5984";
+$HOST = "localhost/aginfra";
+$SERVER_IP = "localhost";
+
+$ES_INDEX = "aginfra_ds";
+?>
